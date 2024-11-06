77b420f2a18b92a61c0f5fb3bea535cc0d53ed1b
Mohammad Haris <112751385+mdharis98@users.noreply.github.com>
2024-11-06 14:18:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
-const outputElement = document.getElementById("output");
-const loadingrow = document.createElement("tr");
-loadingrow.id = "loading";
-loadingrow.innerHTML = '<td colspan="2">Loading...</td>';
-outputElement.appendChild(loadingrow);
-const createPromise = (name) => {
-	const startTime = performance.now();
-	return new Promise((resolve) => {
-		const timeTaken = Math.random() * 2 + 1;
+function manipulateData(inputArray) {
+	return new Promise((resolve, reject) => {
 		setTimeout(() => {
-			const endTime = performance.now();
-			const timeInSeconds = ((endTime - startTime) / 1000).toFixed(3);
-			resolve({ name, time: timeInSeconds });
-		}, timeTaken * 1000);
-	})
+      resolve(inputArray);
+    }, 3000);
+	}).then((array) => {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				const evenNumbers = array.filter((num) => num % 2 === 0);
+				document.getElementById("output").innerText = evenNumbers.join(",");
+				resolve(evenNumbers);
+			},1000);
+		})
+	}).then((evenNumbers)=> {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				const doubleNumbers = evenNumbers.map((num) => num * 2);
+				document.getElementById("output").innerText = doubleNumbers.join(",");
+				resolve(doubleNumbers);
+			},2000);
+		})
+	});
 }
-const promise1 = createPromise("Promise 1");
-const promise2 = createPromise("Promise 2");
-const promise3 = createPromise("Promise 3");
-const startTime = performance.now();
-Promise.all([promise1, promise2, promise3]).then((results) => {
-	outputElement.removeChild(loadingrow);
-	results.forEach((result) => {
-		const row = document.createElement("tr");
-		row.innerHTML = `<td>${result.name}</td><td>${result.time}</td>`;
-		outputElement.appendChild(row);
-	})
-	const endTime = performance.now();
-	const totalTimeInSeconds = ((endTime - startTime) / 1000).toFixed(3);
-	const totalRow = document.createElement('tr');
-	totalRow.innerHTML = `<td>Total</td><td>${totalTimeInSeconds}</td>`;
-	outputElement.appendChild(totalRow);
-})
+
+const inputArray = [1, 2, 3, 4];
+manipulateData(inputArray);
